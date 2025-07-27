ef8708f3adf11fe7e1c67fa03a2b7f992339b6bd
JousSaleh <JoudAlmatrafi@outlook.com>
2025-07-27 04:52:08 +0300

    Add files via upload

---
+++ projectoop/CourseMangment.java
@@ -0,0 +1,10 @@
+
+package projectoop;
+
+
+public interface CourseMangment {
+    // it has the main methods
+    void AssignCourse(Course course, String ChooseTime,Instructor instructor);
+    void RemoveCourse(String CourseN);
+    
+}
